fe2ef0d8f150b83be5ad96495b02de8d272a17cc
zhaokun <283798272@qq.com>
2021-09-17 22:25:51 +0800

    交换机

---
+++ src/main/java/com/example/rabbitmq/_03_publish_subscribe/_02_exchange/Consumer.java
@@ -0,0 +1,48 @@
+package com.example.rabbitmq._03_publish_subscribe._02_exchange;
+
+import com.example.rabbitmq._00_common.RabbitUtils;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ *
+ *
+ * @author Kavin
+ * @date 2021-09-17 16:50:33
+ */
+public class Consumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Producer.class);
+    public static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] args) throws Exception {
+        // 1、获取channel
+        Channel channel = RabbitUtils.createChannel();
+
+        // 2、声明交换机
+        /*
+         * exchange - exchange的名称
+         * type - 交换类型
+         */
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
+
+        // 3、获取临时队列，当消费者断开和该队列的连接时 队列自动删除
+        String queueName = channel.queueDeclare().getQueue();
+
+        // 4、将队列绑定到交换机
+        channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+        // 5、消费消息
+        channel.basicConsume(queueName, true, (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            LOGGER.info("<== 控制台打印接收到的消息" + message);
+        }, consumerTag -> {
+
+        });
+    }
+
+}
---
+++ src/main/java/com/example/rabbitmq/_03_publish_subscribe/_02_exchange/Producer.java
@@ -0,0 +1,54 @@
+package com.example.rabbitmq._03_publish_subscribe._02_exchange;
+
+import com.example.rabbitmq._00_common.RabbitUtils;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+/**
+ * Exchange
+ *     概念：RabbitMQ消息传递模型的核心思想是：[生产者生产的消息从不会直接发送到队列]，相反，[生产者只能将消息发送到交换机]。
+ *          交换机工作的内容非常简单，一方面它接收来自生产者的消息，另一方面将它们放到队列。
+ *          交换机必须确切知道如何处理收到的消息。
+ *          是应该把这些消息放到特定队列，还是把他们放到许多队列中，还是说应该丢弃它们。
+ *          这就的由交换机的类型来决定。
+ *     类型：直接(direct)，主题(topic)，标题(headers)，扇出(fanout)
+ *     默认交换机：将消息根据指定的routingKey(binding key)发送到队列中
+ *     临时队列：每当我们连接到 Rabbit 时，让服务器为我们选择一个随机队列名称，一旦我们断开了消费者的连接，队列将被自动删除。
+ *         String queueName = channel.queueDeclare().getQueue();
+ *     绑定(bindings)：binding 其实是 exchange 和 queue 之间的桥梁，它告诉我们 exchange 和那个 queue 进行了绑定。
+ *     Fanout：
+ *         介绍：它是将接收到的[所有消息][广播]到它知道的[所有队列中]
+ *
+ * @author Kavin
+ * @date 2021-09-17 16:50:33
+ */
+public class Producer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Producer.class);
+    public static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] args) throws Exception {
+        // 1、获取channel
+        Channel channel = RabbitUtils.createChannel();
+
+        // 2、声明交换机
+        /*
+         * exchange - exchange的名称
+         * type - 交换类型
+         */
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
+
+        // 3、发送消息
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String message = scanner.next();
+            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes(StandardCharsets.UTF_8));
+        }
+    }
+
+}
